cac031b5dd676444046a54f98bf94c02ec57b93f
Madeline Stark <madeline.stark1@gmail.com>
2018-09-25 22:32:42 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -35,19 +35,13 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do
-    binding.pry
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"]) #updates owner_id
     if !params["owner"]["name"].empty?
       @owner = Owner.create(name: params["owner"]["name"])
       @owner.pets << @pet #can't just save @pet.owner, need to tell owner about it
-      @pet.owner_id = @owner.id
-    else
-      @owner = Owner.find(params["owner"]["id"])
-      @owner.pets << @pet
-      @pet.owner_id = @owner.id
     end
-    @pet.update
     redirect to "pets/#{@pet.id}"
   end
+
 end
